5b5dd0f01285365264d115f55ecc7cf6f21870ed
keithroe <keithroe@cba41313-dcc7-113d-0d3d-2a2d30939b49>
2010-05-19 21:50:17 +0000

    very broken implementation of type checking via AbstractSyntax::translate

--- rbtiger/lib/Type.rb
+++ rbtiger/lib/Type.rb
@@ -1,2 +1,111 @@
 
+module RBTiger
 
+class Type
+
+  def Type.assert_matches expected, received, lineno
+    raise TypeMismatch( expected, received, lineno ) if( !received.matches( expected ) ) 
+  end
+  
+  def Type.assert_int received, lineno
+    Type.assert_matches INT.new, received, lineno
+  end
+    
+  def Type.assert_unit received, lineno
+    Type.assert_matches UNIT.new, received, lineno
+  end
+  
+  def Type.assert_is_a received, expected_class, lineno
+    raise TypeMismatch( received, expected_class, lineno ) if( !received.is_a?( expected_class ) )
+  end
+    
+  def matches t2
+    return self.is_a?( t2.class )
+  end
+end
+
+
+class INT < Type
+  def initialize
+  end
+end
+
+class STRING < Type
+  def initialize
+  end
+end
+
+
+class RECORD < Type
+
+  attr_accessor :fields
+
+  def initialize( fields )
+    # Hash : Symbol => Type 
+    @fields = fields
+  end
+
+  def matches t2
+    # see comment under ArrayType.matches for explanation
+    return self.equal?( t2 )
+  end
+end
+
+
+class ARRAY < Type
+
+  attr_accessor :elem_type
+
+  def initialize( elem_type)
+    @elem_type = elem_type
+  end
+
+  def matches t2
+    # Array.new should only be called from an ArrayType record type creation
+    # expression (ie, 'array of string').  This ensures each Array type is unique:
+    # type arrtype1 = array of int                # creates unique array type
+    # type arrtype2 = array of int                # creates unique array type
+    # type arrtype3 = arrtype1                    # does NOT create a new type 
+    # var arr1: arrtype1 := arrtype2 [10] of 0    # error, type mismatch
+    # var arr2: arrtype1 := arrtype3 [10] of 0    # valid; arrtype1 same as arrtype3 
+    #
+    # This behavior extends to Records as well
+
+    return self.equal?( t2 )
+  end
+
+end
+
+
+class NIL < Type
+  
+  def initialize
+  end
+
+  def matches t2
+    # nil is a valid record type
+    return ( (t2.is_a? RECORD ) || (self.equal? t2) )
+  end
+end
+
+
+class UNIT < Type
+  def initialize
+  end
+end
+
+
+class NAME < Type
+
+  attr_accessor :type_name
+  attr_accessor :type_ref  # Will be nil for partially resolved types
+
+  def initialize( type_name, type_ref )
+    @type_name = type_name
+    @type_name = type_ref
+    @ordered_vars = %w(@type_name) 
+  end
+end
+
+
+end # module RBTiger
--- rbtiger/lib/rbtiger.rb
+++ rbtiger/lib/rbtiger.rb
@@ -2,5 +2,7 @@
 lib = File.dirname(__FILE__)
 
 require lib + '/AbstractSyntax'
+require lib + '/Exception.rb'
 require lib + '/Parser'
 require lib + '/SymbolTable'
+require lib + '/Type.rb'
