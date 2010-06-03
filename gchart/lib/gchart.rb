ef05be6846019c5d301fb10155c982e2cb5f94bd
keithroe <keithroe@cba41313-dcc7-113d-0d3d-2a2d30939b49>
2010-06-03 00:16:14 +0000

    cleanup

--- gchart/lib/gchart.rb
+++ gchart/lib/gchart.rb
@@ -35,20 +35,20 @@ module GChart
   #############################################################################
   class Data
     attr_accessor :name
-    attr_accessor :data
-    attr_accessor :scale
-    attr_accessor :color
+    attr_accessor :values  # [ v1, v2, ..., vN ]
+    attr_accessor :scale   # [ min, max ]
+    attr_accessor :color   # [ r, g, b ] each in [0, 255]
 
-    def initialize( data )
+    def initialize( values )
       @name       = nil
-      @data       = data
+      @values     = values
       @scale      = nil 
       @color      = [ 128,128,128 ]
     end
 
     def to_s
       s  = "Data '#{name}':\n"
-      s += "  data : #{data.inspect}\n"
+      s += "  data : #{values.inspect}\n"
       s += "  scale: #{scale.inspect}\n"
       s += "  color: #{color.inspect}\n"
     end
@@ -71,15 +71,14 @@ module GChart
     def initialize( which_axis )
       @position        = which_axis  # chxt 'x', 'y', 'r', or 't' 
       @range           = nil         # chxr [ min, max, step ]
-      @labels          = nil         # chxl
-      @label_positions = nil         # chxp
+      @labels          = nil         # chxl [ label1, label2, ..., labelN ] 
+      @label_positions = nil         # chxp 
       @label_style     = nil         # chxs
       @tick_style      = nil         # chxtc
 
     end
 
-    def setRange( *range )   # chxr
-      puts range.inspect
+    def setRange( *range )
       if range.size != 2 && range.size != 3
         raise ArgumentError.new( "setRange must take either [min, max] or [min, max, step]" )
       end
@@ -87,7 +86,7 @@ module GChart
       @labels = nil
     end
 
-    def setLabels( *labels )     # chxl
+    def setLabels( *labels )
       @labels = labels
       @range  = nil
     end
@@ -109,21 +108,102 @@ module GChart
     end
   end
 
-  #############################################################################
+  ##############################################################################
   #
   # Chart 
   #
-  #############################################################################
+  ##############################################################################
   class Chart
 
+    #
+    # Class for storing axes and accumulating their values into url arrays
+    #
+    class AxisSet
+      attr_accessor :axes 
 
+      def initialize
+        @axes = []
+      end
 
-    @@url_header = 'http://chart.apis.google.com/'
+      def push( axis ); @axes.push(axis) end
+
+      def url
+        chxt = Array.new
+        chxr = Array.new
+        chxl = Array.new
+        @axes.each_with_index do |axis, i|
+          chxt.push axis.position
+          chxr.push( ( [i] + axis.range ).join(',') ) if ( axis.range )
+          chxl.push( [i.to_s+":"] + axis.labels )     if ( axis.labels)
+        end
+        s = "&chxt=#{chxt.join(',')}"
+        s+= "&chxr=#{chxr.join('|')}"
+        s+= "&chxl=#{chxl.join('|')}"
+        s
+      end
+    end
+
+    #
+    # Class for storing data series and accumulating their values into url arrays
+    #
+    class DataSet
+      attr_accessor :data
+      attr_accessor :data_type
+      def initialize
+        @data         = Array.new
+        @data_type    = 't' 
+        @data_format  = "%2.3f" 
+        @random_color = true 
+      end
+
+      def push( data ); @data.push( data ) end
+
+      def url( data_format, legend_on ) 
+        chd  = Array.new
+        chds = Array.new
+        chco = Array.new
+        chdl = Array.new
+        min_max = [ 1e8, -1e8 ]
+
+        # process data values
+        @data.each do |data|
+          data_vals = data.values.map! { |x| x ? ( data_format % x ).to_f : nil }
+          min_max = data_vals.inject( min_max )  do |mm,val| 
+            !val ? mm : [ ( mm[0] > val ? val : mm[0] ), ( mm[1] < val ? val : mm[1] ) ]
+          end
+          #TODO: base this on scale, not hardcoded to -1.0
+          data_vals.map! { |a| a == nil ? -1.0 : a }
+          chd.push  data_vals.join( @data_type == 't' ? ',' : '' ) 
+          chds.push data.scale if data.scale 
+          chco.push data.color.map { |x| "%02X" %  x }.join
+          chdl.push data.name
+        end
+
+        # data values
+        s  = "&chd=#{@data_type}:#{chd.join( '|' )}"
+
+        # data scales
+        s += "&chds=#{(chds.size > 0 ? chds : min_max).join( ',' )}"
+
+        # data colors
+        s += "&chco=#{chco.join( ',' )}"
+
+        # data names
+        s += "&chdl=#{chdl.join( '|' )}" if legend_on
+        s
+      end
+    end
+
+    
+    #
+    # Chart class implementation 
+    #
+
+    @@server = 'http://chart.apis.google.com/'
 
     attr_accessor :size
     attr_accessor :type
     attr_accessor :data
-    attr_accessor :data_type
 
 
     def initialize
@@ -133,20 +213,19 @@ module GChart
       @title_size       = nil 
       @title_color      = nil 
 
-      @data_type        = 't' 
-      @data_format      = '%.3f' 
-      @data             = [] 
 
       @legend_on        = false
       @legend_position  = nil 
       @legend_order     = nil 
 
-      @axes             = [] 
+      @axis_set         = AxisSet.new 
+      @data_set         = DataSet.new 
 
       @bg
 
-      @auto_color       = true
+      @data_format      = "%.3f"
     end
+
 
     def legendOn( position = nil, label_order = nil )
       @legend_on       = true
@@ -154,9 +233,11 @@ module GChart
       @legend_order    = label_order
     end
 
+
     def legendOff
       @legend = false 
     end
+
 
     def setChartTitle( title, size = nil, color = nil )
       @title        = title
@@ -164,36 +245,37 @@ module GChart
       @title_color  = color 
     end
 
+
     def setDataColors( *colors )
       # If colors.size is less than number of series, colors are cycled
-
-      if( colors.size > @data.size )
+      
+      if( colors.size > @data_set.data.size )
         raise ArgumentError( "Passed %i colors but only have %i data series" % [ colors.size, @data.size ] )
       end
 
-      @data.each_with_index do |data, i|
+      @data_set.data.each_with_index do |data, i|
         data.color = colors[ i % colors.size ]
       end
     end
 
 
     def addData( data )
-      @data.push Data.new( data )
-      yield @data.last if block_given? 
-      @data.last
+      @data_set.push( Data.new( data ) )
+      yield @data_set.data.last if block_given? 
+      @data_set.data.last
     end
 
 
     def addAxis( which )
       raise ArgumentError.new( "invalid axis parameter" ) if ![ 'x', 'y', 'r', 't' ].include?( which )
-      @axes.push Axis.new( which )
-      yield @axes.last if block_given? 
-      @axes.last
+      @axis_set.push Axis.new( which )
+      yield @axis_set.axes.last if block_given? 
+      @axis_set.axes.last
     end
 
 
     def url
-      s = "#{@@url_header}/chart?cht=#{@type}&chs=#{@size.join('x')}"
+      s = "#{@@server}/chart?cht=#{@type}&chs=#{@size.join('x')}"
 
       if( @title ) 
         s += "&chtt=#{@title.gsub( ' ', '+' ).gsub( "\n", '|' )}"
@@ -203,79 +285,21 @@ module GChart
         end
       end
 
-      chd  = Array.new
-      chds = Array.new
-      chco = Array.new
-      chdl = Array.new
-      min_max = [ 1e8, -1e8 ]
-
-      # process data values
-      @data.each do |data|
-        data_vals = data.data
-        min_max = data_vals.inject( min_max )  do |mm,val| 
-          if( val )
-            val = ( @data_format % val ).to_f
-            [ mm[0] > val ? val : mm[0], mm[1] < val ? val : mm[1] ]
-          else
-            mm if !val
-          end
-        end
-        data_vals.map! { |a| a == nil ? -1.0 : a }
-        data_vals.map! { |data_val| @data_format % data_val.to_f }
-        chd.push data_vals.join( @data_type == 't' ? ',' : '' ) 
-        chds.push data.scale.join(',') if data.scale 
-        chco.push data.color.map { |x| "%02X" % ( @auto_color ? rand(256) :  x ) }.join
-        chdl.push data.name
-      end
-
-      # data values
-      s += "&chd=#{@data_type}:"
-      s += chd.join( '|' )
-
-      # data scales
-      s += '&chds='
-      if chds.size > 0 
-        s += chds.join( '|' )
-      else
-        s += min_max.join( ',' )
-      end
-
-      # data colors
-      s += '&chco='
-      s += chco.join( ',' )
-
-      # data names
-      if @legend_on
-        s += '&chdl='
-        s += chdl.join( '|' )
-      end
       if @legend_position
         legend_order = @legend_order ? "|#{@legend_order}" : '' 
         s += "&chdlp=#{@legend_position}#{legend_order}"
 
       end
 
-      
-      chxt = Array.new
-      chxr = Array.new
-      chxl = Array.new
-      @axes.each_with_index do |axis, i|
-        chxt.push axis.position
-        chxr.push ( ( [i] + axis.range ).join(',') ) if ( axis.range )
-        chxl.push ( [i.to_s+":"] + axis.labels)      if ( axis.labels)
-        puts chxr.last.inspect
-      end
-      s+= "&chxt=#{chxt.join(',')}"
-      s+= "&chxr=#{chxr.join('|')}"
-      s+= "&chxl=#{chxl.join('|')}"
-
+      s += @data_set.url( @data_format, @legend_on )
+      s += @axis_set.url()
+      s
     end
 
 
     def png( out_filename)
       Net::HTTP.start("chart.apis.google.com") do |http|
-        filename =  self.url.gsub( @@url_header, '' )
-        puts " retrieving <#{filename}>"
+        filename =  self.url.gsub( @@server, '' )
         resp = http.get( filename )
         open( out_filename, "wb") do |file|
           file.write(resp.body)
@@ -285,8 +309,5 @@ module GChart
 
   end
 
-
-
 end # GChart
-
 
