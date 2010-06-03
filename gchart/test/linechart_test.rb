01df5c09eeed74a2ff5b5d8b530e058cdab6c86b
keithroe <keithroe@cba41313-dcc7-113d-0d3d-2a2d30939b49>
2010-06-03 00:16:14 +0000

    cleanup

--- gchart/test/linechart_test.rb
+++ gchart/test/linechart_test.rb
@@ -10,13 +10,19 @@ chart.legendOn 't'
 chart.setChartTitle 'test1', 20, [0xff, 0, 0xff]
 
 chart.addData [ 0.0, 0.3, 0.2, 0.5, 0.4, 0.7, 0.6, 0.9 ] do |data|
-  data.name = 'foo'
+  data.name  = 'foo'
+  data.scale = [ 0.0, 1.2 ]
+  data.color = [ 255, 128, 128 ]
 end
 chart.addData [ nil, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1 ] do |data|
   data.name = 'bar'
+  data.scale = [ 0.0, 1.2 ]
+  data.color = [ 255, 128, 128 ]
 end
 chart.addData [ 0.9, 0.6, 0.4, 0.3, 0.4, 0.6, 0.9, nil ] do |data|
   data.name = 'zoo'
+  data.scale = [ 0.0, 1.2 ]
+  data.color = [ 255, 128, 128 ]
 end
 
 chart.addAxis( 'y' ) do |axis|
