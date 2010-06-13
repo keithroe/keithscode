b283b6e0d52171dae3fcfba5196faac2271c7292
keithroe <keithroe@cba41313-dcc7-113d-0d3d-2a2d30939b49>
2010-06-13 21:07:20 +0000

    switched to mkrf for configure

--- roptix/libc/extconf.rb
+++
@@ -1,8 +0,0 @@
-require 'mkmf'
-
-dir_config( 'optix', '/usr/local/optix/include', '/usr/local/optix/lib64' )
-
-exit(1) if !have_header( "optix.h" )
-exit(1) if !have_library( "optix", "rtContextCreate" )
-
-create_makefile( "roptix" )
