d35fcc7bbc02dc67efa1711f5b055dae6f165bbd
keithroe <keithroe@cba41313-dcc7-113d-0d3d-2a2d30939b49>
2010-06-17 22:49:56 +0000

    Fixing linux optix default paths

--- roptix/ext/roptix/mkrf_conf.rb
+++ roptix/ext/roptix/mkrf_conf.rb
@@ -6,15 +6,15 @@ Mkrf::Generator.new( 'roptix_internal' ) do |g|
   g.include_library( 'optix', 'rtContextCreate',
                      "#{ENV['OPTIX_LOCATION']}/lib64",
                      "#{ENV['OPTIX_LOCATION']}/lib",
-                     '/usr/local/OptiX/lib',
-                     '/usr/local/OptiX/lib64',
-                     '/Developer/OptiX/lib',
-                     '/Developer/OptiX/lib64' )
+                     '/usr/local/optix/lib',
+                     '/usr/local/optix/lib64',
+                     '/Developer/optix/lib',
+                     '/Developer/optix/lib64' )
 
   g.include_header( 'optix.h',
                      "#{ENV['OPTIX_LOCATION']}/include",
-                     '/usr/local/OptiX/include',
-                     '/Developer/OptiX/include' )
+                     '/usr/local/optix/include',
+                     '/Developer/optix/include' )
 
   if( !g.has_function?( 'rtContextCreate' ) )
     g.abort!( "Failed to load optix header or library" )
