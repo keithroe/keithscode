98d4519393b3f000a5ce5e4526a9e07116cab08c
keithroe <keithroe@cba41313-dcc7-113d-0d3d-2a2d30939b49>
2012-01-09 19:43:15 +0000

    Clean and doc timer test

--- klib/test/timer.cc
+++ klib/test/timer.cc
@@ -9,40 +9,52 @@ struct AccumulateElapsed
 
     void operator()( double time_elapsed )
     { accumulated_time_elapsed += time_elapsed; }
+
     double accumulated_time_elapsed;
 };
 
+
 int main( int argc, char** argv )
 {
+    // global_timer  will stay on for the entire test
     Timer global_timer;
     global_timer.start();
 
-    unsigned long long total_sleep_ms = 0;
-    double loop_time_elapsed = 0.0;
-
+    // outer_timer will be turned off and on within each loop iteration
     Timer outer_timer;
+    
+    // This var stores accumulated results from loop scope timer, inner_timer 
+    double inner_timer_accumulated_time = 0.0;
+
+    // This struct stores accumulated timer from AutoTimerRef within loop
     AccumulateElapsed ae;
+
     for( int i = 0; i < 100; ++i )
     {
+        // Create our auto timer
         AutoTimerRef<AccumulateElapsed> auto_timer( ae );
+
+        // Start the outer timer
         outer_timer.start();
 
+        // Create and start the inner timer
         Timer inner_timer;
         inner_timer.start();
 
         usleep( 10000 );
-        total_sleep_ms += 10000;
 
-        loop_time_elapsed += inner_timer.getTimeElapsed();
+        // accumulate from inner timer
+        inner_timer_accumulated_time += inner_timer.getTimeElapsed();
 
+        // pause our outer_timer
         outer_timer.stop();
+
+        // lastly, auto timer goes out of scope and invokes callback
     }
 
-    global_timer.stop();
-
-
-    std::cerr << "global_timer: " << global_timer.getTimeElapsed() << std::endl;
-    std::cerr << "auto_timer  : " << ae.accumulated_time_elapsed << std::endl;
-    std::cerr << "outer_timer : " << outer_timer.getTimeElapsed() << std::endl;
-    std::cerr << "inner_timer : " << loop_time_elapsed  << std::endl;
+    // These should all be VERY similar :)
+    std::cout << "global_timer: " << global_timer.getTimeElapsed() << std::endl
+              << "auto_timer  : " << ae.accumulated_time_elapsed   << std::endl
+              << "outer_timer : " << outer_timer.getTimeElapsed()  << std::endl
+              << "inner_timer : " << inner_timer_accumulated_time  << std::endl;
 }
