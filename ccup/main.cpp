18d3c640b494157c0bc64b572eb457db9983dea7
keithroe@gmail.com <keithroe@gmail.com@cba41313-dcc7-113d-0d3d-2a2d30939b49>
2012-10-27 16:12:39 +0000

    Refactored random move selection

--- ccup/main.cpp
+++ ccup/main.cpp
@@ -448,9 +448,9 @@ struct LoopTimerInfo
 //------------------------------------------------------------------------------
 
 
-const int GRID_SIZE      = 15u;
+const int GRID_SIZE      = 15;
 const int NUM_GRID_CELLS = GRID_SIZE*GRID_SIZE;
-const int INVALID_IDX    = 255;
+const int INVALID_IDX    = 16;
 
 
 typedef std::vector< std::pair<int, int> > Move;
@@ -475,6 +475,20 @@ bool legalExploration( const Board& board, Color color, int idx );
 int to1D( int x, int y );
 void to2D( int i, int& x, int& y );
 
+bool chooseExpansion(
+        Color color,
+        const Board& board,
+        const std::vector<int>& random_points,
+        Move& move 
+        );
+
+bool chooseExploration(
+        Color color,
+        const Board& board,
+        std::vector<int>& potential_expolorations,
+        Move& move 
+        );
+
 //------------------------------------------------------------------------------
 //
 // Point
@@ -483,8 +497,8 @@ void to2D( int i, int& x, int& y );
 
 struct Point
 {
-    unsigned char x;
-    unsigned char y;
+    unsigned char x; //  These can be packed into a single byte if necessary
+    unsigned char y; // 
 
     unsigned char color;
     unsigned char group; 
@@ -523,6 +537,9 @@ public:
 
     void setColor( Color c )
     { m_color = c; }
+
+    int numStones( Color color )const
+    { return color == WHITE ? m_num_white_stones : m_num_black_stones; }
 
     int numWhiteStones()const
     { return m_num_white_stones; }
@@ -605,9 +622,6 @@ void Board::set( int x, int y, Color color )
     Point& p = m_grid[ idx ];
     p.color = static_cast<unsigned char>( color ); 
 
-    if( color != m_color )
-        return;
-
     // Assign group to first neighbor group we find
     for( int i = 0; i < 4; ++i )
     {
@@ -615,7 +629,7 @@ void Board::set( int x, int y, Color color )
         if( nidx == INVALID_IDX )
             continue;
 
-        if( m_grid[ nidx ].group != 0 )
+        if( m_grid[ nidx ].color == color && m_grid[ nidx ].group != 0  )
         {
             p.group = m_grid[ nidx ].group;
             break;
@@ -634,6 +648,7 @@ void Board::set( int x, int y, Color color )
     {
         int nidx = p.neighbors[ i ];
         if( nidx != INVALID_IDX             &&
+            m_grid[ nidx ].color == color   &&
             m_grid[ nidx ].group != 0       &&
             m_grid[ nidx ].group != p.group )
         {
@@ -775,18 +790,6 @@ public:
             Move& move
             );
 protected:
-    // TODO:Specialize this for MCTS since it cant use persistant m_explorations
-    virtual void chooseExploration(
-            Color color,
-            const Board& board,
-            Move& move
-            );
-
-    virtual void chooseExpansion(
-            Color color,
-            const Board& board,
-            Move& move
-            );
 
     std::vector<int> m_explorations;
     std::vector<int> m_expansions;
@@ -812,150 +815,20 @@ void RandomAI::chooseMove(
         Move& move 
         )
 {
-    if( board.numWhiteStones() == 0 || drand48() < 0.5f )
-        chooseExploration( color, board, move );
+    if( board.numStones( color ) == 0 || drand48() < 0.5f )
+    {
+        if( !chooseExploration( color, board, m_explorations, move ) )
+            chooseExpansion( color, board, m_expansions, move );
+    }
     else
-        chooseExpansion( color, board, move );
-    //chooseExploration( color, board, move );
-}
-
-
-void RandomAI::chooseExploration(
-        Color color,
-        const Board& board,
-        Move& move 
-        )
-{
-    move.clear();
-
-    // Walk the vector till we find the first legal move
-    while( !m_explorations.empty() )
     {
-        const int idx = m_explorations.back();
-        m_explorations.pop_back();
-
-        if( legalExploration( board, color, idx ) )
-        {
-            int x, y;
-            to2D( idx, x, y );
-            move.push_back( std::make_pair( x, y ) );
-            return;
-        }
+        if( !chooseExpansion( color, board, m_expansions, move ) )
+            chooseExploration( color, board, m_explorations, move );
     }
-
-    // No legal exploration, fall back to expansion
-    chooseExpansion( color, board, move );
 }
 
 
-void RandomAI::chooseExpansion(
-        Color color,
-        const Board& board,
-        Move& move 
-        )
-{
-    LDEBUG << "Choosing exansion *************************";
-    assert( board.numWhiteStones() != 0 );
-    
-    move.clear();
-    std::set< int > expansions;
 
-    int adjacent_groups[4];
-
-    // Iterate over points in random order
-    for( std::vector<int>::iterator it = m_expansions.begin();
-         it != m_expansions.end();
-         ++it )
-    {
-        const int idx = *it;
-        const Point& p = board.get( idx );
-
-        int x, y;
-        to2D( idx, x, y );
-        LDEBUG << "considering " << x << "," << y;
-        if( p.color == NONE ) // empty point
-        {
-            LDEBUG << "    it is empty";         
-
-            // Find this points adjacent groups
-            int num_adjacent = 0;
-            for( int i = 0; i < 4; ++i )
-            {
-                int nidx = p.neighbors[ i ];
-                if( nidx == INVALID_IDX )
-                    continue;
-
-                const Point& neighbor_p = board.get( nidx );
-                if( neighbor_p.color == color )
-                {
-                    int ngroup = neighbor_p.group;
-
-                    // Avoid redundant group entries
-                    if( ( num_adjacent < 1 || adjacent_groups[0] != ngroup ) &&
-                        ( num_adjacent < 2 || adjacent_groups[1] != ngroup ) && 
-                        ( num_adjacent < 3 || adjacent_groups[2] != ngroup ) ) 
-                        adjacent_groups[ num_adjacent++ ] = neighbor_p.group;
-                }
-            }
-
-            // Not on border of any group -- skip it
-            if( num_adjacent == 0 )
-            {
-                LDEBUG << "       no adj groups, skipping";
-                continue;
-            }
-
-            int x, y;
-            to2D( idx, x, y );
-            const std::pair<int, int> coord = std::make_pair( x, y );
-
-            // If we have a single adjacent group
-            if( num_adjacent == 1 )
-            {
-                int group = adjacent_groups[0];
-                if( expansions.find( group ) != expansions.end() )
-                {
-                    LDEBUG << "         group already expanded, skipping";
-                    continue;
-                }
-
-                LDEBUG << "        inserting ";
-                expansions.insert( group ); 
-                move.push_back( coord );
-                continue;
-            }
-
-            if( num_adjacent > 1 )
-            {
-                bool ok_to_join = true;
-                for( int i = 0; i < num_adjacent; ++i )
-                {
-
-                    if( expansions.count( adjacent_groups[ i ] ) > 0 )
-                    {
-                        ok_to_join = false;
-                        break;
-                    }
-                }
-                if( ok_to_join )
-                {
-                    for( int i = 0; i < num_adjacent; ++i )
-                    {
-                        expansions.insert( adjacent_groups[ i ] ); 
-                    }
-                    move.push_back( coord );
-                    LDEBUG << "       >1 adj groups, works!!";
-                }
-                continue;
-            }
-
-            LDEBUG << "       >1 adj groups, skipping";
-        }
-    }
-
-    if( move.empty() )
-        chooseExploration( color, board, move );
-}
 
 //------------------------------------------------------------------------------
 //
@@ -1083,7 +956,7 @@ void MCTSAI::chooseMove(
             for( Move::iterator it = move.begin(); it != move.end(); ++it )
                 sim_board.set( it->first, it->second, cur_color );
 
-            cur_color = cur_color == WHITE : BLACK ? WHITE;
+            cur_color = (cur_color == WHITE ? BLACK : WHITE );
         }
         
 
@@ -1297,6 +1170,144 @@ void to2D( int i, int& x, int& y )
 {
     x = i / GRID_SIZE;
     y = i % GRID_SIZE;
+}
+
+bool chooseExploration(
+        Color color,
+        const Board& board,
+        std::vector<int>& potential_explorations,
+        Move& move 
+        )
+{
+    LDEBUG << "Choosing exploration *************************";
+    move.clear();
+
+    // Walk the vector till we find the first legal move
+    while( !potential_explorations.empty() )
+    {
+        const int idx = potential_explorations.back();
+        potential_explorations.pop_back();
+
+        if( legalExploration( board, color, idx ) )
+        {
+            int x, y;
+            to2D( idx, x, y );
+            move.push_back( std::make_pair( x, y ) );
+            return true;
+        }
+    }
+
+    // No legal exploration found
+    return false;
+
+}
+
+bool chooseExpansion(
+        Color color,
+        const Board& board,
+        const std::vector<int>& random_points,
+        Move& move 
+        )
+{
+    LDEBUG << "Choosing exansion *************************";
+    assert( board.numStones( color ) != 0 );
+    
+    move.clear();
+    std::set< int > expansions;
+
+    int adjacent_groups[4];
+
+    // Iterate over points in random order
+    for( std::vector<int>::const_iterator it = random_points.begin();
+         it != random_points.end();
+         ++it )
+    {
+        const int idx = *it;
+        const Point& p = board.get( idx );
+
+        int x, y;
+        to2D( idx, x, y );
+        if( p.color == NONE ) // empty point
+        {
+            LDEBUG << "considering " << x << "," << y;
+
+            // Find this points adjacent groups
+            int num_adjacent = 0;
+            for( int i = 0; i < 4; ++i )
+            {
+                int nidx = p.neighbors[ i ];
+                if( nidx == INVALID_IDX )
+                    continue;
+
+                const Point& neighbor_p = board.get( nidx );
+                if( neighbor_p.color == color )
+                {
+                    int ngroup = neighbor_p.group;
+
+                    // Avoid redundant group entries
+                    if( ( num_adjacent < 1 || adjacent_groups[0] != ngroup ) &&
+                        ( num_adjacent < 2 || adjacent_groups[1] != ngroup ) && 
+                        ( num_adjacent < 3 || adjacent_groups[2] != ngroup ) ) 
+                        adjacent_groups[ num_adjacent++ ] = neighbor_p.group;
+                }
+            }
+
+            // Not on border of any group -- skip it
+            if( num_adjacent == 0 )
+            {
+                LDEBUG << "       no adj groups, skipping";
+                continue;
+            }
+
+            int x, y;
+            to2D( idx, x, y );
+            const std::pair<int, int> coord = std::make_pair( x, y );
+
+            // If we have a single adjacent group
+            if( num_adjacent == 1 )
+            {
+                int group = adjacent_groups[0];
+                if( expansions.find( group ) != expansions.end() )
+                {
+                    LDEBUG << "         group already expanded, skipping";
+                    continue;
+                }
+
+                LDEBUG << "        inserting ";
+                expansions.insert( group ); 
+                move.push_back( coord );
+                continue;
+            }
+
+            if( num_adjacent > 1 )
+            {
+                bool ok_to_join = true;
+                for( int i = 0; i < num_adjacent; ++i )
+                {
+
+                    if( expansions.count( adjacent_groups[ i ] ) > 0 )
+                    {
+                        ok_to_join = false;
+                        break;
+                    }
+                }
+                if( ok_to_join )
+                {
+                    for( int i = 0; i < num_adjacent; ++i )
+                    {
+                        expansions.insert( adjacent_groups[ i ] ); 
+                    }
+                    move.push_back( coord );
+                    LDEBUG << "       >1 adj groups, works!!";
+                }
+                continue;
+            }
+
+            LDEBUG << "       >1 adj groups, skipping";
+        }
+    }
+
+    return( !move.empty() );
 }
 
 //------------------------------------------------------------------------------
